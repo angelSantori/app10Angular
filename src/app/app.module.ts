1ca22861bad2fc45df80fb024bcefc672b10d388
angelSantori <antonyo.da.mm@gmail.com>
2023-11-05 16:28:32 -0700

    Agregadi httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http' //<--- Agregado
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    ,HttpClientModule //<--- Agregado
   ],
   providers: [],
   bootstrap: [AppComponent]
